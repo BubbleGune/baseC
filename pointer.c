5d1d1e2d247dd0a9e1de6ed9be4cc3d285c2bc64
ub <3kosko@gmail.com>
2022-04-29 21:13:49 +0200

    pointer

--- pointer.c
+++ pointer.c
@@ -1,31 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-void GetPointer();
-void RemoveElemArr(int *a, int *pn, int x);
+#define SIZE 20
 
+void GetPointer();
+void PrintStr(char *str);
 int main()
 {
-	int a[] = {1, 3, 4,4, 1,1, 6};
-	int i, size;
-	size = sizeof(a) / sizeof(*a);
-	RemoveElemArr(a, &size, 4);
-
-	for (int i = 0; i < size; i++)
-		printf("%d\n", a[i]);
+	char str[SIZE] = { "I am  learn prog"};
+	char *p_str[SIZE];
+	for (int i = 0; i < SIZE; i++){
+		p_str[i] = &str[i];
+		printf("%s", p_str[i]);
+	}
+	putchar('\n');
 	return 0;
-}
-
-void RemoveElemArr(int *a, int *pn, int x)
-{
-	int i, j;
-	for (int i = 0; i < *pn && a[i] != x; i++)
-		for (int j = i+1; j < *pn; j++){
-			 if(a[j] != x)
-				a[i++] = a[j];	
-		}	
-	// new size of array
-	*pn = j;
 }
 
 void GetPointer()
@@ -43,4 +32,18 @@ void GetPointer()
 		if (*p_name == 'a') *p_name = 'A';
 	}	
 	putchar('\n');
+}
+void PrintArInt(int *ar, int n)
+{
+	for (int i = 0; i < n; i++){
+		printf("%d\t", ar[i]);
+	}
+	printf("\n");
+}
+void PrintStr(char *str)
+{
+	while (*str != '\0'){
+		fprintf(stdout, "%c", *str);		
+		*str++;
+	}	
 }
