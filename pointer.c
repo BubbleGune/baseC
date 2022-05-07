07abff2f38d83dd97f9ed5d17ae17d76356cf489
ub <3kosko@gmail.com>
2022-05-07 16:20:06 +0200

    pointers

--- pointer.c
+++ pointer.c
@@ -1,49 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-#define SIZE 20
-
-void GetPointer();
-void PrintStr(char *str);
 int main()
 {
-	char str[SIZE] = { "I am  learn prog"};
-	char *pStr[SIZE];
-	for (int i = 0; i < SIZE; i++){
-		pStr[i] = &str[i];
-		printf("%s", pStr[i]);
-	}
-	putchar('\n');
-	return 0;
-}
-
-void GetPointer()
-{
-	int number = 10;
-	int *pNum = &number;
-	printf("%p\t%d\n", pNum, *pNum);
-
-	char name[] = "myname";
-	char *pName = name;
+	int a = 10;
+	int *b = &a;
 	
-	// string is cannot be change, but with pointer...
-	for(int i = 0; i < strlen(name); i++){
-		fprintf(stdout, "%c", *pName++);
-		if (*pName == 'a') *pName = 'A';
-	}	
-	putchar('\n');
-}
-void PrintArInt(int *ar, int n)
-{
-	for (int i = 0; i < n; i++){
-		printf("%d\t", ar[i]);
-	}
-	printf("\n");
-}
-void PrintStr(char *str)
-{
-	while (*str != '\0'){
-		fprintf(stdout, "%c", *str);		
-		*str++;
-	}	
+	printf("a = %d\t", a);
+	printf("b = %d\n", *b);
+	printf("&a = %p\t", &a);
+	printf("&b = %p\n", b);
+	return 0;
 }
